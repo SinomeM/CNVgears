f5738c0b6d13303918075008fffbf4d43b63cfb0
Simone Montalbano <simone.montalbano@protonmail.com>
2020-09-01 15:58:56 +0200

    removed rare_CNVs.R as the process can be easily be descibed in the vignette

--- R/rare_CVNs.R
+++
@@ -1,6 +0,0 @@
-
-# compute CNVRs frequency in the population considering the total cohort
-# size and select the calls from "rare" CNVRs, e.g. CNVRs with a frequency < 1%
-# or even 0.1%
-
-# in addtion, integrate also the information from DGV and similar
